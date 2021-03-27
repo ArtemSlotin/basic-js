c2f2f63b9576a06850761a053a388c2ae0e87ed5
Artem Slotin <66305683+ArtemSlotin@users.noreply.github.com>
2021-03-27 08:44:12 +0200

    DONE: count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(backyard) {
+  let result = [];
+  let ears = '^^';
+  let count;
+
+  for (let place of backyard) {
+    count = place.filter(elem => elem == ears);
+    result = result.concat(count);
+  }
+  return result.length;
+  
 };
