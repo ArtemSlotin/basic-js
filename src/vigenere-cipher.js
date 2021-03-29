707c310e113b8a19c866c4b6637fec4492a1ff7c
Artem Slotin <66305683+ArtemSlotin@users.noreply.github.com>
2021-03-29 22:35:36 +0300

    Feat: implement class constructor Vigenere encoder

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,44 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error');
+
+const ABC = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(normal = true) {
+    this.isReversed = !normal;
+  }
+
+  encrypt(m, k) {
+    const message = m.toUpperCase();
+    const key = k.repeat(Math.ceil(m.length / k.length)).toUpperCase();
+    let result = '';
+    let keyInd = 0;
+    for (let i = 0; i < message.length; i++) {
+      if (ABC.includes(message[i])) {
+        const shift = ABC.indexOf(message[i]) + ABC.indexOf(key[keyInd]);
+        result += shift <= 25 ? ABC[shift] : ABC[shift - 25 - 1];
+        keyInd++;
+      } else result += message[i];
+    }
+
+    if (this.isReversed) return result.split('').reverse().join('');
+    return result;
+  }
+
+  decrypt(m, k) {
+    const message = m.toUpperCase();
+    const key = k.repeat(Math.ceil(m.length / k.length)).toUpperCase();
+    let result = '';
+    let keyInd = 0;
+    for (let i = 0; i < message.length; i++) {
+      if (ABC.includes(message[i])) {
+        const shift = ABC.indexOf(message[i]) - ABC.indexOf(key[keyInd]);
+        result += shift < 0 ? ABC[shift + 26] : ABC[shift];
+        keyInd++;
+      } else result += message[i];
+    }
+
+    if (this.isReversed) return result.split('').reverse().join('');
+    return result;
   }
 }
 
