2df6c82cd836cb6702a553e1735ed7c7fcf3f6dd
Artem Slotin <66305683+ArtemSlotin@users.noreply.github.com>
2021-03-29 21:05:38 +0300

    Feat: implement extended-repeater function

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,70 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error');
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  const mainStr = '' + str;
+  const mainSequence = [];
+  const addSequence = [];
+  const [repeatTimes, separator, additionSeparator] = [
+    options.repeatTimes || 1,
+    options.separator || '+',
+    options.additionSeparator || '|',
+  ];
+
+  if ('addition' in options) {
+    const [addition, additionRepeatTimes] = [options.addition, options.additionRepeatTimes || 1];
+    for (let i = 0; i < additionRepeatTimes; i++) {
+      addSequence.push('' + addition);
+    }
+
+    if (repeatTimes === 1) return mainStr + addSequence.join(additionSeparator);
+  }
+
+  for (let i = 0; i < repeatTimes; i++) {
+    mainSequence.push(mainStr + addSequence.join(additionSeparator));
+  }
+
+  return mainSequence.join(separator);
 };
+
+
+// const CustomError = require("../extensions/custom-error");
+
+// module.exports = function repeater(str, options) {
+//   let result = str;
+//   let {
+//     repeatTimes  = 0,
+//     separator = '+',
+//     additionSeparator  = '|',
+//     additionRepeatTimes,
+//     addition
+//   } = options;
   
+//   if (typeof str !== 'string' && typeof addition !== 'string' && typeof addition == 'number') {
+//     str = str.toString();   
+//     addition = addition.toString();
+//   }
+    
+//   if (repeatTimes === undefined || additionRepeatTimes === undefined) {
+//     if (typeof addition == 'object' && addition == null) {
+//        addition = '' + addition;
+//     }
+//     if (addition) {
+//       result += `${addition}`;
+//     }
+//   }
+  
+//   for (let i = 1; i <= repeatTimes; i++) {
+//     for (let k = 1; k <= additionRepeatTimes; k++) {
+//       if (k == additionRepeatTimes) {
+//         result += `${addition}`;
+//       } else result =`${result}${addition}${additionSeparator}`;
+//     }
+//           console.log(i == repeatTimes)
+
+//     if (i == repeatTimes) {
+//       null;
+//     } else result += `${separator}${str}`;
+//   }
+//   console.log(result)
+//   return result;
+// };
