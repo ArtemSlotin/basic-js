a4691f0fed3c3c3772cb0110f5b3c5795569c51d
Artem Slotin <66305683+ArtemSlotin@users.noreply.github.com>
2021-03-29 21:30:39 +0300

    Feat: implement function transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,29 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+if (!Array.isArray(arr)) throw Error;
+
+let temporary = [...arr];
+
+temporary.forEach((item, i, arr) => {
+  switch(item) {
+   case '--discard-next':
+    arr[i] = arr[i + 1] = undefined;
+   break;
+
+   case '--discard-prev':
+    arr[i] = arr[i - 1] = undefined;
+   break;
+
+   case '--double-next':
+    arr[i] = arr[i + 1];
+   break;
+
+   case '--double-prev':
+    arr[i] = arr[i - 1];
+   break;
+  }
+})
+
+return temporary.filter(x => typeof x !== 'undefined');
 };
